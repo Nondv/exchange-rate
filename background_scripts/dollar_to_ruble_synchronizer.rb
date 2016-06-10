bb672c561a2d24d746a388a7781080fc5632f541
Dmitriy Non <non.dmitriy@gmail.com>
2016-06-11 02:05:48 +0300

    parse rate from RBC site

--- background_scripts/dollar_to_ruble_synchronizer.rb
+++ background_scripts/dollar_to_ruble_synchronizer.rb
@@ -1,10 +1,26 @@
+require 'open-uri'
+
 SLEEP_TIME = 5
+
 @rate = ExchangeRate.find_by(from: :dollar, to: :ruble)
 raise('run seeds first!') unless @rate
 
+
 def update_currency
-  # TODO
-  @rate.update_attributes(value: rand(200))
+  new_value = usd_rate_from_cbr
+  @rate.update_attributes(value: new_value) if new_value
+end
+
+def usd_rate_from_cbr
+  # XML with many currencies
+  uri = URI.parse('http://www.cbr.ru/scripts/XML_daily.asp')
+  rate_list = Hash.from_xml(uri.read)
+
+  usd = rate_list['ValCurs']['Valute'].find { |e| e['CharCode'] == 'USD' }
+  usd && usd['Value'].sub(',', '.').to_f
+rescue # just to KISS
+  Rails.logger.error "Can't get USD rate from CBR"
+  nil
 end
 
 loop do
